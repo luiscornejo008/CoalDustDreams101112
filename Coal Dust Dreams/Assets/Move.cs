a8f2a4f99dc5fd9ecf02187afa86f8d12dd8cec1
eyesofgore <pngbrain@gmail.com>
2024-04-03 13:30:28 -0500

    yed

--- Coal Dust Dreams/Assets/Move.cs
+++ Coal Dust Dreams/Assets/Move.cs
@@ -2,31 +2,125 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Move: MonoBehaviour
-{
-    Rigidbody2D rigidbody2d;
-    float horizontal;
-    float vertical;
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CapsuleCollider2D))]
 
-    // Start is called before the first frame update
+public class Move : MonoBehaviour
+{
+    // Move player in 2D space
+    public float maxSpeed = 3.4f;
+    public float shiftSpeed = 5.4f;
+    public float shiftJump = 20.4f;
+    public float jumpHeight = 6.5f;
+    public float gravityScale = 1.5f;
+    public Camera mainCamera;
+
+    bool facingRight = true;
+    float moveDirection = 0;
+    bool isGrounded = false;
+    Vector3 cameraPos;
+    Rigidbody2D r2d;
+    CapsuleCollider2D mainCollider;
+    Transform t;
+
+    // Use this for initialization
     void Start()
     {
-        rigidbody2d = GetComponent<Rigidbody2D>();
+        t = transform;
+        r2d = GetComponent<Rigidbody2D>();
+        mainCollider = GetComponent<CapsuleCollider2D>();
+        r2d.freezeRotation = true;
+        r2d.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+        r2d.gravityScale = gravityScale;
+        facingRight = t.localScale.x > 0;
+
+        if (mainCamera)
+        {
+            cameraPos = mainCamera.transform.position;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
+        
+        
+        // Movement controls
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            moveDirection = Input.GetKey(KeyCode.A) ? -1 : 1;
+        }
+        else
+        {
+            if (isGrounded || r2d.velocity.magnitude < 0.01f)
+            {
+                moveDirection = 0;
+            }
+        }
+
+        // Change facing direction
+        if (moveDirection != 0)
+        {
+            if (moveDirection > 0 && !facingRight)
+            {
+                facingRight = true;
+                t.localScale = new Vector3(Mathf.Abs(t.localScale.x), t.localScale.y, transform.localScale.z);
+            }
+            if (moveDirection < 0 && facingRight)
+            {
+                facingRight = false;
+                t.localScale = new Vector3(-Mathf.Abs(t.localScale.x), t.localScale.y, t.localScale.z);
+            }
+        }
+
+        // Jumping
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+            {
+                r2d.velocity = new Vector2(r2d.velocity.x, shiftJump);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        {
+            r2d.velocity = new Vector2(r2d.velocity.x, jumpHeight);
+        }
+
+
+        // Camera follow
+        if (mainCamera)
+        {
+            mainCamera.transform.position = new Vector3(t.position.x, t.position.y, cameraPos.z);
+        }
     }
 
     void FixedUpdate()
     {
-        Vector2 position = rigidbody2d.position;
-        position.x = position.x + 7.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 7.0f * vertical * Time.deltaTime;
+        Bounds colliderBounds = mainCollider.bounds;
+        float colliderRadius = mainCollider.size.x * 0.4f * Mathf.Abs(transform.localScale.x);
+        Vector3 groundCheckPos = colliderBounds.min + new Vector3(colliderBounds.size.x * 0.5f, colliderRadius * 0.9f, 0);
+        // Check if player is grounded
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheckPos, colliderRadius);
+        //Check if any of the overlapping colliders are not player collider, if so, set isGrounded to true
+        isGrounded = false;
+        if (colliders.Length > 0)
+        {
+            for (int i = 0; i < colliders.Length; i++)
+            {
+                if (colliders[i] != mainCollider)
+                {
+                    isGrounded = true;
+                    break;
+                }
+            }
+        }
 
-        rigidbody2d.MovePosition(position);
+        // Apply movement velocity
+        r2d.velocity = new Vector2((moveDirection) * maxSpeed, r2d.velocity.y);
+
+        // Simple debug
+        Debug.DrawLine(groundCheckPos, groundCheckPos - new Vector3(0, colliderRadius, 0), isGrounded ? Color.green : Color.red);
+        Debug.DrawLine(groundCheckPos, groundCheckPos - new Vector3(colliderRadius, 0, 0), isGrounded ? Color.green : Color.red);
     }
 }
