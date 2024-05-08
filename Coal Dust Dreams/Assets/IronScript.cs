4d72063be0199f972522e3e4cb6e61dd12264729
Luis <111524258+luiscornejo008@users.noreply.github.com>
2024-05-08 13:28:22 -0500

    I hate ni

--- Coal Dust Dreams/Assets/IronScript.cs
+++ Coal Dust Dreams/Assets/IronScript.cs
@@ -1,21 +1,21 @@
 using UnityEngine;
 using System.Collections;
 
-public class diamondScript : MonoBehaviour
+public class IronScript : MonoBehaviour
 {
     public AudioClip collectedClip;
     void Start()
     {
-        
+
     }
     void OnTriggerEnter2D(Collider2D co)
     {
 
         GameObject pick = GameObject.FindWithTag("Player");
-        
+
         if (co.name == "pickaxe")
         {
-            pick.GetComponent<Move>().getMoney(500);
+            pick.GetComponent<Move>().getMoney(50);
             Destroy(gameObject);
 
         }
